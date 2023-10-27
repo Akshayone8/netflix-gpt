f06b6013fba3baff4aa19d40a732a904543124da
Akshay <62467248+Akshayone8@users.noreply.github.com>
2023-10-27 10:52:16 +0530

    added-form-validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/Validate";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleButtonClick = () => {
+    //validate the form data
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message);
+  };
 
   const toggleSignInform = () => {
     //basicaly it is like a toggle feature if it is true then it will be false and vice versa
@@ -18,7 +29,10 @@ const Login = () => {
           alt="login-background"
         />
       </div>
-      <form className="absolute p-12  my-36 mx-auto left-0 right-0 w-3/12 text-white bg-black  bg-opacity-80">
+      <form
+        className="absolute p-12  my-36 mx-auto left-0 right-0 w-3/12 text-white bg-black  bg-opacity-80"
+        onSubmit={(e) => e.preventDefault()}
+      >
         <h1 className="font-bold text-3xl py-4">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
@@ -30,16 +44,22 @@ const Login = () => {
           />
         )}
         <input
+          ref={email}
           type="text"
           placeholder="Email Address"
           className="p-4 my-4 w-full bg-gray-800"
         />
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className="p-4 my-4 w-full bg-gray-800"
         />
-        <button className="p-4 my-6 bg-red-700 w-full rounded-lg">
+        <p className="text-red-500 font-bold p-1 text-lg">{errorMessage}</p>
+        <button
+          className="p-4 my-6 bg-red-700 w-full rounded-lg"
+          onClick={handleButtonClick}
+        >
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
         <p className="py-4 cursor-pointer" onClick={toggleSignInform}>
---
+++ src/utils/Validate.js
@@ -0,0 +1,16 @@
+export const checkValidData = (email, password) => {
+  const isEmailValid =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+      email
+    );
+
+  const isPasswordValid =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])[A-Za-z\d@.#$!%*?&]{8,10}$/.test(
+      password
+    );
+
+  if (!isEmailValid) return "Email id is Not Valid";
+  if (!isPasswordValid) return "Please check your Password";
+
+  return null;
+};
